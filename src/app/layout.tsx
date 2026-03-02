238f3643f94b85c23972fea4f4846c00adeca232
A.R. Jibon <atikurrahaman386@gmail.com>
2026-03-03 05:07:50 +0600

    feat: add Header component to the root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { EB_Garamond } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const ebGaramond = EB_Garamond({
   variable: "--font-eb-garamond",
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${ebGaramond.variable} antialiased`}>
-        <span className="font-sans">{children}</span>
+        <span className="font-sans">
+          <Header />
+          {children}
+        </span>
       </body>
     </html>
   );
