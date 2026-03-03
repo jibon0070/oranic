4b8e8cc5cf54d43a8abc54f0cfc0033afc9ac4ac
A.R. Jibon <atikurrahaman386@gmail.com>
2026-03-03 13:29:36 +0600

    minus icon

---
+++ src/icons/minus.tsx
@@ -0,0 +1,19 @@
+export default function Minus({ className }: { className?: string }) {
+  return (
+    <svg
+      width="16"
+      height="2"
+      viewBox="0 0 16 2"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+    >
+      <path
+        d="M1 1H15"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
