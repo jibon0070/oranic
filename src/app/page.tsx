78d7304ee41dbd8f3a4398ed748b7ebcbffc0043
A.R. Jibon <atikurrahaman386@gmail.com>
2026-03-06 00:58:09 +0600

    feat: integrate Merquee component into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./_components/hero";
+import Merquee from "./_components/merquee";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Merquee />
     </main>
   );
 }
